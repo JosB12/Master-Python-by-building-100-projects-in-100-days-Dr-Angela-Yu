3c6e3382850b162a8762aedd65f98d5d914928ac
josue blanco <blancojosue931@gmail.com>
2025-08-28 23:59:46 -0400

    - Day 2
    - Identifying errors and data types.
    
    - Converting Data types

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,20 @@
-len("Hello")
+#Subscripting
+print("Hello"[-2])
+
+#concatenation with number strings
+print("1242142" + "123123")
+
+#concatenation with numbers int is an suma
+print(1232142 + 12312412)
+
+print("Jos"[1])
+
+#separatos instead of , comas
+print(123_124_214_214)
+
+#Float
+print(3.1416)
+
+#Booleans
+print(True)
+print(False)
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,28 @@
-len(12345)
+#pause 1
+print(type("abc"))
+
+print(len("avc"))
+
+#pause2
+#int
+# print(type(123))
+
+#string
+# print(type("Hello"))
+
+#Float
+# print(type(123.35))
+
+#type Conversion
+# print(int("123") + int("123"))
+
+#Pause 3 challenge
+# name = len(input("What is your name?"))
+
+nameOfTheUser = input("Enter your name: ")
+lengthOfTheName = str(len(nameOfTheUser))
+
+print(type(nameOfTheUser))#str
+print(type(lengthOfTheName)) #str
+
+print("Number of letters in your name: " + lengthOfTheName)
