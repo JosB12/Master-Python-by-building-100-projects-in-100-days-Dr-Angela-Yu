c190f06d7534b7c8cae751fc4e7f02aa9a487c89
josue blanco <blancojosue931@gmail.com>
2025-08-29 00:36:05 -0400

    - Day 2 - task 3 mathematics and PEMDAS

--- Day 2/Mathematical Operations/task.py
+++ Day 2/Mathematical Operations/task.py
@@ -1,1 +1,40 @@
-print("My age: " + str(12))
+# print("My age: " + str(12))
+#
+# #operation marks
+# print(2 + 2)
+# print (2 - 2)
+# print (3 * 3)
+# print (5 / 3)
+# print (5 // 3)
+# print (4 ** 2)
+# print (2%2)
+# # PEMDAS
+#
+# # ()
+# # **
+# # * Or /
+# # + Or -
+# #Parentheses, Exponents, Multiplication/Division, Addition/Subtraction
+#
+# print(3 * 3 + 3 / 3 -3 )
+#
+# # (3 * 3) + (3 / 3 ) - (3)
+#
+print(3 * 2 - 3)
+print(3 * 3 + 3/2 - 7.5)
+print()
+print(3 * (3 + 3) / 3 - 3)
+
+
+#Code Challege
+# The body mass index (BMI) is a measure used in medicine to see if someone is underweight or overweight. This is the formula used to calculate it:
+# bmi is equal to the person's weight divided by the person's height squared.
+
+height = 1.65
+weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = (weight) / height**2
+
+print(bmi)
