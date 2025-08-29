d22adad7d70f9705b1036846d4bb5c584adedb78
josue blanco <blancojosue931@gmail.com>
2025-08-29 09:53:24 -0400

    - Day 3 - If Else with python

--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -4,8 +4,11 @@ tip = int(input("What percentage tip would you like to give? 10 12 15? "))
 people = int(input("How many people to split the bill? "))
 
 
-tip_Amount = float(1 + tip / 100)
+# tip_Amount = float(1 + tip / 100)
+tip_Amount = float(1+ tip / 100)
+tip_amoun_Total = tip_Amount * bill
 total_amount = bill * tip_Amount / people
+
 total_amount_rounded = round(total_amount, 2)
 
 print(f"Each person should pay: ${total_amount_rounded}")
