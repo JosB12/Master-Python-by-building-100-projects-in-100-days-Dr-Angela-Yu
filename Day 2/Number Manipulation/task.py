ecdb7187a0db5da83d57041413c95f756f436008
josue blanco <blancojosue931@gmail.com>
2025-08-29 01:01:16 -0400

    - Day 2 - task 4 mathematics

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,38 @@
 bmi = 84 / 1.65 ** 2
+
+print(int(bmi)) # remove all the decimal numbers after the dot
+
+print(round(bmi)) #round the decimal number to the highes
+
+print(round(bmi, 2)) #round the number with 2 decimals to the right
+
+
+#assigments operators
+
+bmi += 13
+print(bmi)
+
+bmi -= 13
+print(bmi)
+
+bmi *= 13
+print(bmi)
+
+bmi /= 13
+print(bmi)
+
+
+#f-Strings
+age = 20
+name = "Josue"
+is_human = True
+
+print(f" Hello my name is {name} i'm {age} years old i'm {is_human} in Dominican republic. ")
+
+#Challenge
+# You are a computer. What will this line of code print?
+
+print(6 + 4 / 2 - (1 * 2))
+
+#answer 6
+
