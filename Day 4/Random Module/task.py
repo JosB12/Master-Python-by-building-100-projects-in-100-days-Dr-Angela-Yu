2bd35f6dae975acb5fb926a336c17f274ea54e02
josue blanco <blancojosue931@gmail.com>
2025-08-31 16:21:40 -0400

    - Day 4 - coin flip game other way

--- Day 4/Random Module/task.py
+++ Day 4/Random Module/task.py
@@ -13,9 +13,19 @@ import my_module
 # random_float = random.uniform(0,10)
 # print(random_float)
 
-random_coin_flip = random.random() * 11
-print(random_coin_flip)
-if random_coin_flip <= 3 or random_coin_flip >= 8 and random_coin_flip <= 9 or random_coin_flip == 6:
-    print("Heads")
+# random_coin_flip = random.random() * 11
+# print(random_coin_flip)
+# if random_coin_flip <= 3 or random_coin_flip >= 8 and random_coin_flip <= 9 or random_coin_flip == 6:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+
+random_num1 = 1
+random_num2 = 0
+random_number = random.randint(random_num2, random_num1)
+print(random_number)
+if random_number == 1:
+    print("heads")
 else:
     print("Tails")
