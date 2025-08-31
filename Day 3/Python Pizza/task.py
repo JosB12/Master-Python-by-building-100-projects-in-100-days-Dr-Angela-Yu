2904a7cc9a523d51622542ab59e74b7ddac10d53
josue blanco <blancojosue931@gmail.com>
2025-08-30 23:57:15 -0400

    - Day 3 - Adding nested if

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -8,11 +8,11 @@ elif size == "M":
 elif size == "L":
     pizza_price = 25
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-if size == "S" and pepperoni == "Y":
-    pizza_price += 2
-elif size == "M" or size == "L" and pepperoni == "Y":
-    pizza_price += 3
-
+if pepperoni == "Y":
+    if size == "S":
+        pizza_price += 2
+    else:
+        pizza_price += 3
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 if extra_cheese == "Y":
     pizza_price += 1
