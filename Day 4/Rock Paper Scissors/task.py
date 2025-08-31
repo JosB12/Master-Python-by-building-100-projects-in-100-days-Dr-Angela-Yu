a312eddc008282dd07461313a2306ee3aa5ee387
josue blanco <blancojosue931@gmail.com>
2025-08-31 18:22:43 -0400

    - Day 4 - rock paper scissor game

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +25,61 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+list_chooses = [rock, paper, scissors]
+choose_computer = random.choice(list_chooses)
+
+user_choose = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if user_choose == 0:
+    print(rock)
+    if choose_computer == rock:
+        print("Computer chose:\n")
+        print(rock)
+        print("is a draw")
+    elif choose_computer == paper:
+        print("Computer chose:\n")
+        print(paper)
+        print("you lose")
+    elif choose_computer == scissors:
+        print("Computer chose:\n")
+        print(scissors)
+        print("Your win")
+    else:
+        print("You typed an invalid number, you lose!")
+elif user_choose == 1:
+    print(paper)
+    if choose_computer == rock:
+        print("Computer chose:\n")
+        print(rock)
+        print("Your win")
+
+    elif choose_computer == paper:
+        print("Computer chose:\n")
+        print(paper)
+        print("is a draw")
+
+    elif choose_computer == scissors:
+        print("Computer chose:\n")
+        print(scissors)
+        print("you lose")
+    else:
+        print("You typed an invalid number, you lose!")
+
+elif user_choose == 2:
+    print(scissors)
+    if choose_computer == rock:
+        print("Computer chose:\n")
+        print(rock)
+        print("you lose")
+
+    elif choose_computer == paper:
+        print("Computer chose:\n")
+        print(paper)
+        print("Your win")
+
+    elif choose_computer == scissors:
+        print("Computer chose:\n")
+        print(scissors)
+        print("is a draw")
+    else:
+        print("You typed an invalid number, you lose!")
