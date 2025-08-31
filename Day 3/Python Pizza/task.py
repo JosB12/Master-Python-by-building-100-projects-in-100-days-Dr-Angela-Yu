73b0c4c06cb60b9792a18ca1685809f27ed75e3d
josue blanco <blancojosue931@gmail.com>
2025-08-30 23:53:26 -0400

    - Day 3 - pizza project with elif if else nested and multi if

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -1,5 +1,21 @@
 print("Welcome to Python Pizza Deliveries!")
+pizza_price = 0
 size = input("What size pizza do you want? S, M or L: ")
+if size == "S":
+    pizza_price = 15
+elif size == "M":
+    pizza_price = 20
+elif size == "L":
+    pizza_price = 25
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+if size == "S" and pepperoni == "Y":
+    pizza_price += 2
+elif size == "M" or size == "L" and pepperoni == "Y":
+    pizza_price += 3
+
 extra_cheese = input("Do you want extra cheese? Y or N: ")
+if extra_cheese == "Y":
+    pizza_price += 1
+print(f"Your final bill is: ${pizza_price}.")
+
 
